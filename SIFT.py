1cb84bc66058c935732147e164e7c818eae6be95
toda-hibiki <43288669+toda-hibiki@users.noreply.github.com>
2019-10-02 11:01:38 +0900

    Update SIFT.py

--- SIFT.py
+++ SIFT.py
@@ -20,7 +20,7 @@ matches = bf.knnMatch(des1,des2, k=2)
 good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
-        good.append([m])
+        good.append(m)
 
 
 if len(good)>MIN_MATCH_COUNT:
